e4d7f5c1b84fd88ef6311b44900f3170a657af80
awatani <168165920+awatanitomoru@users.noreply.github.com>
2024-10-22 17:03:33 +0900

    Install expo-router

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
 
 
 export default [
@@ -17,4 +17,4 @@ export default [
       "comma-dangle": [2, "never"]
     }
   }
-];
+]
