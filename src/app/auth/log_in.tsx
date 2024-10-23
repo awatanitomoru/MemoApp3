60996a925ee3ecf583ab396254122f94447c356d
awatani <168165920+awatanitomoru@users.noreply.github.com>
2024-10-23 15:33:43 +0900

    Login styling

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -6,16 +6,16 @@ const Login = (): JSX.Element => {
     return (
         <View style={styles.container}>
             <Header />
-            <View>
-                <Text>Log in</Text>
-                <TextInput value="Email adress" />
-                <TextInput value='password' />
-                <View>
-                    <Text>Submit</Text>
+            <View style={styles.inner}>
+                <Text style={styles.title}>Log in</Text>
+                <TextInput style={styles.input} value="Email adress" />
+                <TextInput style={styles.input} value='password' />
+                <View style={styles.button}>
+                    <Text style={styles.buttonLabel}>Login</Text>
                 </View>
-                <View>
-                    <Text>Not registerd</Text>
-                    <Text>Sign up here!</Text>
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>Not registerd</Text>
+                    <Text style={styles.footerLink}>Sign up here!</Text>
                 </View>
             </View>
         </View>
@@ -24,7 +24,55 @@ const Login = (): JSX.Element => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#F0F4F8'
+    },
+    inner: {
+        paddingVertical: 24,
+        paddingHorizontal: 27
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginBottom: 24
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+        backgroundColor: '#FFFFFF',
+        height: 48,
+        padding: 8,
+        fontSize: 16,
+        marginBottom: 16
+    },
+    button: {
+        backgroundColor: '#76F8AC',
+        borderRadius: 4,
+        alignSelf: 'flex-start',
+        marginBottom: 24
+    },
+    buttonLabel: {
+        fontSize: 16,
+        lineHeight: 32,
+        color: '#000000',
+        paddingVertical: 8,
+        paddingHorizontal: 24
+    },
+    footer: {
+        flexDirection: 'row'
+    },
+    footerText: {
+        fontSize: 14,
+        lineHeight: 24,
+        marginBottom: 8,
+        color: '#000000',
+        paddingHorizontal: 8
+    },
+    footerLink :{
+        fontSize: 14,
+        lineHeight: 24,
+        color: '#76F8AC'
     }
 })
 
