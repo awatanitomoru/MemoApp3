f32d5d6a78d09aef1aa64b8bc26a47d8153b7efc
awatani <168165920+awatanitomoru@users.noreply.github.com>
2024-10-24 15:21:07 +0900

    firebase

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -3,6 +3,7 @@ import {
     TouchableOpacity, StyleSheet
 } from 'react-native'
 import { Link, router } from 'expo-router'
+import { useState } from 'react'
 
 import Button from '../../components/Button'
 
@@ -12,12 +13,29 @@ const handlePress = (): void => {
 }
 
 const Signup = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Signup</Text>
-                <TextInput style={styles.input} value="Email adress" />
-                <TextInput style={styles.input} value='password' />
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => { setEmail(text) }}
+                    autoCapitalize='none'
+                    keyboardType='email-address'
+                    placeholder='Email Address'
+                    textContentType='emailAddress'
+                />
+                <TextInput style={styles.input}
+                    value={password}
+                    onChangeText={(text) => { setPassword(text) }}
+                    autoCapitalize='none'
+                    secureTextEntry
+                    placeholder='Password'
+                    textContentType='password'
+                />
                 <Button label='Sign up' onPress={handlePress}/>
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Alreadey registered?</Text>
